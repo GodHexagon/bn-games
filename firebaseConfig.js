512add162a2a3df5a4bba6f69691d3c4e08ea49c
GodHexagon <god.hexagon.bifry@gmail.com>
2023-12-03 09:57:05 +0900

    firebaseをbn-gamesプロジェクトで初期化

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBU1I9oPnHrr_zwk2C-fbsFrBe5gYGHgAU",
-  authDomain: "qtr-test.firebaseapp.com",
-  projectId: "qtr-test",
-  storageBucket: "qtr-test.appspot.com",
-  messagingSenderId: "119821397007",
-  appId: "1:119821397007:web:44ecf36f260451c0a3877a"
+  apiKey: "AIzaSyDiOhIJ-J2EkxkRTBIoN1yjTtbHNdA1JCU",
+  authDomain: "bn-games-2403f.firebaseapp.com",
+  projectId: "bn-games-2403f",
+  storageBucket: "bn-games-2403f.appspot.com",
+  messagingSenderId: "447248708299",
+  appId: "1:447248708299:web:bd4fe3cadf869a1eefa14e"
 };
 
 // Initialize Firebase
