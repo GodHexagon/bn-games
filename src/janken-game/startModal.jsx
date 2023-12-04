103ead1d4e24013822b78a5cb2d5df15e5beb311
GodHexagon <god.hexagon.bifry@gmail.com>
2023-12-04 23:07:49 +0900

    modified:   src/janken-game/startModal.jsx Modalのスタイルをpaperにした

--- src/janken-game/startModal.jsx
+++ src/janken-game/startModal.jsx
@@ -1,25 +1,18 @@
 import { useState } from 'react'
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import Paper from '@mui/material/Paper';
 
-const style = {
+const paperStyle = {
   position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
   p: 4,
 };
 
 function StartModal() {
   const [open, setOpen] = useState(true);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
 
   return  (
     <>
@@ -28,14 +21,9 @@ function StartModal() {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
+        <Paper sx={paperStyle}>
+          
+        </Paper>
       </Modal>
     </>
   )
