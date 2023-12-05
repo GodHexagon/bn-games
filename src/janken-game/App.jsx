9962a2f929498680ff955213b17ab6c84464678f
とおる <133332942+GodHexagon@users.noreply.github.com>
2023-12-05 16:53:53 +0900

    マッチング機能を編集

--- src/janken-game/App.jsx
+++ src/janken-game/App.jsx
@@ -1,10 +1,9 @@
 // react and firebase
 import { useState } from 'react'
 import db from "./../../firebaseConfig"
-import {doc, getDoc, setDoc } from "firebase/firestore"
+import {doc, getDocs, addDoc, collection, query, where } from "firebase/firestore"
 //contents
 import reactLogo from './../assets/react.svg'
-// styles
 // components
 import StartModal from './startModal'
 // mui componetns
@@ -13,9 +12,27 @@ import IconButton from '@mui/material/IconButton';
 
 function App() {
   const [openStartModal, setOpenStartModal] = useState(true);
+
+  const resolvePassword = async (password) => {
+    const q = query(collection(db, "active_sessions"), where("password", "==", password), where("player", "==", 1));
+    const querySnapshot = await getDocs(q);
+    var gettedPassword;
+    querySnapshot.forEach((doc) => {
+      console.log("matched session:", doc.id, " => ", doc.data());
+      gettedPassword = doc.data().password;
+    });
+
+    if(gettedPassword === undefined){
+      const docRef = await addDoc(collection(db, "active_sessions"), {
+        password: password,
+        player: 1
+      });
+      console.log("create new session:", docRef)
+    }
+  };
   const hSubmitPassword = (e, password) => {
-    console.log(password);
-  }
+    resolvePassword(password);
+  };
 
   return (
     <>
