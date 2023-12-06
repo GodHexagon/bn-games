82fad94e543c73f84f2856247019cc6d834a2a29
GodHexagon <god.hexagon.bifry@gmail.com>
2023-12-06 23:24:00 +0900

    セッション管理を更新

--- src/janken-game/App.jsx
+++ src/janken-game/App.jsx
@@ -11,115 +11,63 @@ import Container from '@mui/material/Container';
 import IconButton from '@mui/material/IconButton';
 
 const playerID = crypto.randomUUID();
-var usPings = [];
 var usSession;
 var ownSessionID = "";
-var replyed = true;
+var isOver = false
+
+const onHostChange = (e) => {
+  console.log("session update:", e.id, "|", e.data())
+  isOver = e.data().player_id.length > 2
+  if(isOver){
+    console.log("session is full over, number of player:", e.data().player_id.length)
+  }
+
+  
+}
+
+const subSession = (newID) => {
+  setTimeout(() => {
+    usSession = onSnapshot(doc(db, "active_sessions", newID), (e) => {
+      onHostChange(e);
+    });
+  }, 100);
+}
+
+const tryPassword = async (password) => {
+  // 合言葉でDBを検索し変数に格納
+  const q = query(collection(db, "active_sessions"), where("password", "==", password));
+  const querySnapshot = await getDocs(q);
+  var matched;
+  querySnapshot.forEach((doc) => {
+    console.log("matched session:", doc.id, " => ", doc.data());
+    matched = doc;
+  });
+
+  if(matched === undefined){
+    // 見つからなかったら新規作成
+    const docRef = await addDoc(collection(db, "active_sessions"), {
+      password: password,
+      player_id: [playerID],
+    });
+    ownSessionID = docRef.id;
+    subSession(docRef.id);
+
+    console.log("create new session:", docRef.id)
+  }else{
+    // 見つかったら名簿に追加
+    if(matched.data().player_id.length < 2){
+      const docMatched = doc(db, "active_sessions", matched.id);
+      await updateDoc(docMatched, {
+        player_id: arrayUnion(playerID)
+      });
+    }
+    ownSessionID = matched.id;
+    subSession(matched.id);
+  }
+}
+
 function App() {
   const [openStartModal, setOpenStartModal] = useState(true);
-
-  const resReply = async (id, livings) => {
-    // フレッシュな値でDBを更新
-    replyed = true;
-    const docRef = await updateDoc(doc(db, "active_sessions", id), {
-      playerID: livings
-    }, { merge: true });
-    // 満員判定
-    if(livings.length < 2){
-      await updateDoc(doc(db, "active_sessions", id), {
-        playerID: [...livings, playerID]
-      }, { merge: true });
-      ownSessionID = id;
-      console.log("joining succesed:", id)
-    }else{
-      console.log("session is full:", id);
-    }
-  }
-
-  // 合言葉を使用したセッション管理関数
-  const resolvePassword = async (password) => {
-    // 合言葉でDBを検索しfindDocに格納
-    const q = query(collection(db, "active_sessions"), where("password", "==", password));
-    const querySnapshot = await getDocs(q);
-    var matched;
-    querySnapshot.forEach((doc) => {
-      console.log("matched session:", doc.id, " => ", doc.data());
-      matched = doc;
-      //doc.data().playerID
-    });
-
-    if(matched === undefined){
-      // 見つからなかったら新規作成
-      const docRef = await addDoc(collection(db, "active_sessions"), {
-        password: password,
-        playerID: [playerID]
-      });
-      ownSessionID = docRef.id;
-
-      console.log("create new session:", docRef.id)
-    }else{
-      // 見つかったら既存プレイヤー全員の生存を確認
-      var livings = [];
-      const exisPlayers = matched.data().playerID;
-      replyed = false;
-      // タイムアウトした
-      setTimeout(() => {
-        if(!replyed){
-          resReply(matched.id, livings)
-          exisPlayers.forEach((id) => {
-            deleteDoc(doc(db, "player_ping", id));
-          })
-        }
-      }, 2000);
-      exisPlayers.forEach(async (id, index) => {
-        const pingDoc = doc(db, "player_ping", id);
-        await setDoc(pingDoc, {
-          joining: "?"
-        });
-        usPings.push(onSnapshot(pingDoc, (document) => {
-          console.log("got reply:", document.id, "->", document.data())
-          if(matched.id == document.data().joining){
-            livings.push(document.id);
-            console.log("living player:", livings)
-          }
-          if(usPings.length == livings.length){
-            // 全員分の応答があった
-            resReply(matched.id, livings);
-          }
-          usPings[index]();
-        }));
-      });
-    }
-  };
-  const tryPassword = async (password) => {
-    // 合言葉でDBを検索し変数に格納
-    const q = query(collection(db, "active_sessions"), where("password", "==", password));
-    const querySnapshot = await getDocs(q);
-    var matched;
-    querySnapshot.forEach((doc) => {
-      console.log("matched session:", doc.id, " => ", doc.data());
-      matched = doc;
-    });
-    if(matched === undefined){
-      // 見つからなかったら新規作成
-      const docRef = await addDoc(collection(db, "active_sessions"), {
-        password: password,
-        player_id: [playerID],
-      });
-      ownSessionID = docRef.id;
-
-      console.log("create new session:", docRef.id)
-    }else{
-      const docMatched = doc(db, "active_sessions", matched.id);
-      const usMatched = onSnapshot(docMatched, (e) => {
-        console.log("matched session:", e.id, "|", e.data())
-      });
-      await updateDoc(docMatched, {
-        playerID: arrayUnion(playerID)
-      });
-    }
-  }
-
   // joinボタンを押したイベントハンドラ
   const hSubmitPassword = (e, password) => {
     tryPassword(password);
