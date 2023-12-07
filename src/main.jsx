8486c97bee7cdebcfbf3a33024b96de052789b14
GodHexagon <god.hexagon.bifry@gmail.com>
2023-12-07 22:42:10 +0900

    簡易なバージョンで作成

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Janken from './janken-game/App'
+import Janken from './janken-s-game/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
