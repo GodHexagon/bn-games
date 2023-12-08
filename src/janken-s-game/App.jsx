03e86d78b9bb6f3c1e9b2f4cb94e1174ef91daa2
GodHexagon <god.hexagon.bifry@gmail.com>
2023-12-08 21:50:03 +0900

    ogpを修正

--- src/janken-s-game/App.jsx
+++ src/janken-s-game/App.jsx
@@ -1,6 +1,5 @@
 import DocumentMeta from 'react-document-meta';
 import Page from './Page';
-import imageUri from './../assets/scissors.png'
 
 function App(){
   const meta = {
@@ -10,13 +9,8 @@ function App(){
       property: {
         'og:title': 'Janken S | BN Games ~ 学習用Reactアプリ',
         'og:url': 'https://bn-games-2403f.web.app/janken-s',
-        'og:image': imageUri,
+        'og:image': 'https://bn-games-2403f.web.app/assets/scissors-SHJFQtfI.png',
         'og:description': '同じ合言葉の相手と対戦できます。合言葉とじゃんけんを入力した後、「Submit!」ボタンを押して自分のじゃんけんを確定できます。',
-
-        'twitter:card': 'summary_large_image',
-        'twitter:title': 'Janken S | BN Games ~ 学習用Reactアプリ',
-        'twitter:description': '同じ合言葉の相手と対戦できます。合言葉とじゃんけんを入力した後、「Submit!」ボタンを押して自分のじゃんけんを確定できます。',
-        'twitter:image': imageUri
       }
     }
   };
